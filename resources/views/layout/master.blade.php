9727701cab68c675392648713773a6848932b46c
Prasetyanto <powerbet31@gmail.com>
2024-02-21 21:28:33 +0700

    Build frontend

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('assets/css/build.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Pictura</title>
+</head>
+<body class="scrollbar-hidden bg-backcolor">
+
+</body>
+</html>
